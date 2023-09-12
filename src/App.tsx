96c20e6bfb0c1867887d33380738ebb21e28c096
davidrogger <davidrogger.a@gmail.com>
2023-09-12 16:54:45 -0300

    WIP: aside bar elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { ModeToggle } from '@/components/mode-toggle';
-import { Github } from 'lucide-react';
+import { FileVideo, Github, Upload } from 'lucide-react';
 import { Separator } from '@/components/ui/separator';
 import { Button } from '@/components/ui/button';
+import { Textarea } from './components/ui/textarea';
 
 export function App() {
   return (
@@ -42,8 +43,70 @@ export function App() {
         </div>
       </header>
 
-      <main>
+      <main className='flex-1 p-6 flex gap-6 max-[640px]:flex-col-reverse'>
+        <section
+          className='flex flex-col flex-1 gap-4'
+        >
+          <div
+            className='grid grid-rows-2 gap-4 flex-1'
+          >
+            <Textarea
+              className='resize-none p-4 leading-relaxed'
+              placeholder='Add IA prompt...'
+            />
+            <Textarea
+              className='resize-none p-4 leading-relaxed'
+              placeholder='ChatGPT answer...'
+              readOnly
+            />
+            <p
+              className='text-sm text-muted-foreground'
+            >
+              Remember: You can use the
+              <span className='text-violet-400'>{' {transciption} '}</span>
+              variable in the prompt to add more content to the transcrition from the selected video.
+            </p>
+          </div>
+        </section>
+        <aside className='w-80 max-[640px]:m-auto space-y-6'>
+          <form
+            className='space-y-6'
+          >
+            <label 
+              htmlFor="video"
+              className='border flex rounded-md aspect-video cursor-pointer border-dashed text-sm flex-col gap-2 items-center justify-center text-muted-foreground hover:bg-primary/5'
+            >
+              <FileVideo className='w-4 h-4' />
+              Select a Video
+            </label>
 
+            <input type="file" id="video" accept="video/mp4" className='sr-only' />
+            
+            <Separator />
+
+            <section
+              className='space-y-2'
+            >
+              <label htmlFor="transcription_prompt">
+                Transcription Prompt
+              </label>
+              <Textarea
+                id="trabscription_prompt"
+                className='h-20 leading-relaxed resize-none'
+                placeholder='Add keywords mentioned in the video by comma (,)'
+              />
+            </section>
+
+            <Button
+              type='submit'
+              className='w-full'
+            >
+                Load Video
+              <Upload className='w-4 h-4 ml-2' />
+            </Button>
+          </form>
+
+        </aside>
       </main>
     </div>
   );
