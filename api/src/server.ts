428bb7af824c222000b1a8f0596592a7ba82f863
davidrogger <davidrogger.a@gmail.com>
2023-09-13 12:13:19 -0300

    BUILD: install cors

--- api/src/server.ts
+++ api/src/server.ts
@@ -3,10 +3,16 @@ import { getAllPromptsRoute } from './routes/get-all-prompts';
 import { uploadVideoRoute } from './routes/upload-video';
 import { createTranscriptionRoute } from './routes/create-transcripton';
 import { generateCompletionRoute } from './routes/generate-ai-completetion';
+import fastifyCors from '@fastify/cors';
 
 const app = fastify();
 
 const PORT:number = Number(process.env.PORT) || 3333;
+const PATH_URL = process.env.PATH_URL || '*';
+
+app.register(fastifyCors, {
+  origin: PATH_URL
+});
 
 app.register(getAllPromptsRoute);
 app.register(uploadVideoRoute);
