ae85c5ab67a7d378f4e1f538c310c350403c18d4
davidrogger <davidrogger.a@gmail.com>
2023-09-13 10:50:56 -0300

    FEAT: upload audio route

---
+++ api/src/routes/status.ts
@@ -0,0 +1,3 @@
+export enum StatusCode {
+  BAD_REQUEST = 400
+}
--- api/src/server.ts
+++ api/src/server.ts
@@ -1,11 +1,13 @@
 import { fastify } from 'fastify';
 import { getAllPromptsRoute } from './routes/get-all-prompts';
+import { uploadVideoRoute } from './routes/upload-video';
 
 const app = fastify();
 
 const PORT:number = Number(process.env.PORT) || 3333;
 
 app.register(getAllPromptsRoute);
+app.register(uploadVideoRoute);
 
 app
   .listen({ port: PORT })
